3711957db3abb8462319e447e5865bc53de76ddf
Zachary Skalko <zskalko@gmail.com>
2016-08-19 22:25:05 -0500

    add sass middlewear

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var app = express();
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
 
+//pug renderer, index
+app.set('views', './views')
+app.set('view engine', 'pug')
 app.get('/', function (req, res) {
   res.render('index', {});
 });
 
-app.set('views', './views')
-
-app.set('view engine', 'pug')
-
-
-
+//static xlsx
 app.use('/xlsx.js', express.static(__dirname + '/node_modules/xlsx/dist/xlsx.core.min.js'));
 
-app.use(express.static('public'));
+//sass
+app.use(sassMiddleware({
+    src: __dirname,
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'extended'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//listen
 app.listen(3000, function () {
   console.log('localhost:3000');
 });
