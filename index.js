f1b3ae8008a209876b678c31aa9fa3217c7428d9
Zachary Skalko <zskalko@gmail.com>
2016-08-19 21:51:19 -0500

    Install pug middlewear

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
+
+app.set('views', './views')
+
+app.set('view engine', 'pug')
+
+
+
 app.use('/xlsx.js', express.static(__dirname + '/node_modules/xlsx/dist/xlsx.core.min.js'));
 
 app.use(express.static('public'));
